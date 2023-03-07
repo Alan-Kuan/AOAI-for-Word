39e3664a32e47426e2265025d56de08d38d264c8
Alan-Kuan <kuanhaoan@gmail.com>
2023-03-07 11:41:37 +0800

    chore: add vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+import '@mdi/font/css/materialdesignicons.css';
+import '@/assets/main.css';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
 
 window.Office.onReady(() => {
-    createApp(App).mount('#app')
-})
+    createApp(App)
+        .use(vuetify)
+        .mount('#app');
+});
