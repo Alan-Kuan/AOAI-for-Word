7548566a2536a87086e131f53c666bb1b207ba45
Alan-Kuan <kuanhaoan@gmail.com>
2023-03-08 15:41:49 +0800

    feat: add FoldableSection

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
-import WritingStyleTrans from '@/components/WritingStyleTrans.vue';
 import Notification from '@/components/Notification.vue';
+import WritingStyleTrans from '@/components/WritingStyleTrans.vue';
 </script>
 
 <template>
   <v-layout>
-    <v-app-bar color="#4fd594" elevation="0">
-      <v-app-bar-title class="text-white">
-        Word OpenAI
-      </v-app-bar-title>
-    </v-app-bar>
     <v-main>
       <Notification />
       <WritingStyleTrans />
--- src/components/WritingStyleTrans.vue
+++ src/components/WritingStyleTrans.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue';
 import * as openai from '@/libs/openai.js';
 import { notify } from '@/libs/notify.js';
-import FeatureCard from '@/components/FeatureCard.vue';
+import FoldableSection from './FoldableSection.vue';
 
 const default_prompt = ref('請用白話文改寫：');
 const inferencing = ref(false);
@@ -36,16 +36,20 @@ function onConvert() {
 </script>
 
 <template>
-  <FeatureCard title="換句話說">
-    <v-textarea label="提詞前綴" v-model="default_prompt" />
-    <v-btn
-      class="w-100"
-      color="blue"
-      :loading="inferencing"
-      @click="onConvert"
-    >
-      <v-icon icon="mdi-fountain-pen-tip" />
-      轉換
-    </v-btn>
-  </FeatureCard>
+  <FoldableSection title="換句話說">
+    <v-card class="mb-3" flat>
+      <v-card-text>
+        <v-textarea label="提詞前綴" v-model="default_prompt" />
+        <v-btn
+          class="w-100"
+          color="blue"
+          :loading="inferencing"
+          @click="onConvert"
+        >
+          <v-icon icon="mdi-fountain-pen-tip" />
+          轉換
+        </v-btn>
+      </v-card-text>
+    </v-card>
+  </FoldableSection>
 </template>
