925357b1b15f3d657ac9324e7cacb5c80fa1e2f8
Alan-Kuan <kuanhaoan@gmail.com>
2023-03-12 22:27:27 +0800

    feat: log notification message too

--- src/libs/notify.js
+++ src/libs/notify.js
@@ -4,9 +4,10 @@ export const show = ref(false);
 export const message = ref('');
 
 export function notify(msg, timeout=3000) {
-  show.value = true;
-  message.value = msg;
-  setTimeout(() => show.value = false, timeout);
+    console.log(msg);
+    show.value = true;
+    message.value = msg;
+    setTimeout(() => show.value = false, timeout);
 }
 
 export function dismiss() {
