5def14283d0c12706e117db43c7fb29fd9b1a985
Alan-Kuan <kuanhaoan@gmail.com>
2023-06-05 18:19:46 +0800

    chore: separate code about vuetify from the main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-
-import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-
-import '@mdi/font/css/materialdesignicons.css';
-import '@/assets/main.css';
-
-const vuetify = createVuetify({
-    components,
-    directives,
-});
+import vuetify from './vuetify.js';
 
 Office.onReady(() => {
         createApp(App)
