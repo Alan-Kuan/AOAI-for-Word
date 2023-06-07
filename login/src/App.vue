1ac4ba7febb4b918e633b28788be6080b93ba1a9
Alan-Kuan <kuanhaoan@gmail.com>
2023-06-07 16:01:52 +0800

    feat: add English translation to login page

--- login/src/App.vue
+++ login/src/App.vue
@@ -1,5 +1,28 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import { signIn } from './auth.js';
+
+const lang_idx = ref(0);
+
+const login_texts = ['登入', 'Login'];
+const login_hints = [
+  '使用你的組織帳號登入',
+  "Login with your organization's account",
+];
+
+function getLanguageIndex() {
+  const lang_idx = localStorage.getItem('language') ?? '0';
+  return parseInt(lang_idx, 10);
+}
+
+function setLanguageIndex(idx) {
+  lang_idx.value = idx;
+  localStorage.setItem('language', idx);
+}
+
+onMounted(() => {
+  lang_idx.value = getLanguageIndex();
+});
 </script>
 
 <template>
@@ -24,12 +47,30 @@ import { signIn } from './auth.js';
           color="blue"
           @click="signIn"
         >
-          登入
+          {{ login_texts[lang_idx] }}
         </v-btn>
       </div>
       <div class="mt-5 text-caption text-grey">
-        Microsoft Internal Account Only
+        {{ login_hints[lang_idx] }}
+      </div>
+      <div class="mt-16">
+        <v-btn-toggle
+          v-model="lang_idx"
+          selected-class="selected-lang"
+          color="blue"
+          :mandatory="true"
+          variant="plain"
+        >
+          <v-btn @click="setLanguageIndex(0)">中文</v-btn>
+          <v-btn @click="setLanguageIndex(1)">English</v-btn>
+        </v-btn-toggle>
       </div>
     </v-container>
   </v-layout>
 </template>
+
+<style scoped>
+.selected-lang {
+  font-weight: bold;
+}
+</style>
